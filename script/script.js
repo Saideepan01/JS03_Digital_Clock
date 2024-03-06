c2c0d7d19f39fb30e6b4a93aada1fd002e07f47a
Saideepan01 <saideepan0v@gmail.com>
2024-03-06 23:46:38 +0530

    Complete Clock Project

---
+++ script/script.js
@@ -0,0 +1,43 @@
+const clock = document.querySelector('.clock');
+
+function runClock(){
+    var time = new Date();
+    var hrs = time.getHours();
+    var mins = time.getMinutes();
+    var sec = time.getSeconds();
+    var txt = "AM";
+    if(hrs >12){
+        hrs-=12;
+        txt="PM";
+    }else if(hrs==0){
+        hrs ="12";
+        txt="AM";
+    }
+    hrs = hrs < 10? '0'+ hrs:hrs;
+    mins = mins < 10? '0'+ mins:mins;
+    sec = sec < 10? '0'+ sec:sec;
+    clock.innerHTML=`${hrs} : ${mins} : ${sec} ${txt}`;
+
+    //---------------------------------------------
+    const h = document.querySelectorAll('.h')
+    const m = document.querySelectorAll('.m')
+    const s = document.querySelectorAll('.s')
+
+    hrs = hrs.toString();
+    mins = mins.toString();
+    sec = sec.toString();
+
+    h[0].innerHTML=hrs[0];
+    h[1].innerHTML=hrs[1];
+
+    m[0].innerHTML=mins[0];
+    m[1].innerHTML=mins[1];
+
+    s[0].innerHTML=sec[0];
+    s[1].innerHTML=sec[1];
+}
+
+
+
+runClock();
+setInterval(runClock, 1000);
