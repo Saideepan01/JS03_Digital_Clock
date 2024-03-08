727d529d4e34769d34949624a07a2e9f2e2c6da4
Saideepan01 <saideepan0v@gmail.com>
2024-03-08 11:09:00 +0530

    complete digital clock v1

--- script.js
+++ script.js
@@ -17,8 +17,27 @@ function runClock(){
     mins = mins < 10? '0'+ mins:mins;
     sec = sec < 10? '0'+ sec:sec;
     clock.innerHTML=`${hrs} : ${mins} : ${sec} ${txt}`;
-    
+
+    //---------------------------------------------
+    /*
+    const h = document.querySelectorAll('.h')
+    const m = document.querySelectorAll('.m')
+    const s = document.querySelectorAll('.s')
+
+    hrs = hrs.toString();
+    h[0].innerHTML=hrs[0];
+    h[1].innerHTML=hrs[1];
+
+    mins = mins.toString();
+    m[0].innerHTML=mins[0];
+    m[1].innerHTML=mins[1];
+
+    sec = sec.toString();
+    s[0].innerHTML=sec[0];
+    s[1].innerHTML=sec[1];
+    */
 }
+
 
 runClock();
 setInterval(runClock, 1000);
